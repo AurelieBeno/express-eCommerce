e5a5597e2d07fb1a92ae6a8c0b747db781333f39
unknown <aurelie.benovici@gmail.com>
2019-03-01 12:14:42 +0100

    auth user in progress

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
+const cors = require("cors");
+const session = require("express-session");
+const MongoStrore = require("connect-mongo");
+const passport = require("passport");
 
+require("./config/passport-setup.js");
 mongoose
   .connect("mongodb://localhost/express-ecommmerce", { useNewUrlParser: true })
   .then(x => {
@@ -28,5 +33,37 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  cors({
+    // receive cookies
+    credentials: true,
+    // only there domains/origins can access
+    originin: ["http://localhost:3000"]
+  })
+);
+
+app.use(
+  session({
+    //default setting
+    resave: true,
+    saveUninitialized: true,
+
+    // session secret
+    secret: process.env.SESSION_SECRET,
+    store: new MongoStrore({ mongooseConnection: mongoose.connection })
+  })
+);
+
+// Activate some of the passport methods in our routes
+app.use(passport.initialize());
+
+// Load the logged-in user's information once we are logged-in
+app.use(passport.session());
+
+const auth = require("./routes/auth-router.js");
+// All routes in the auth router will start with "/api"
+// (example: "/logout -> "/api/logout")
+app.use("/api", auth);
 
 module.exports = app;
