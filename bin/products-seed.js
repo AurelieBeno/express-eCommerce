f3e2c09d43063546e9fa836af3f3c7b0e657b80e
unknown <aurelie.benovici@gmail.com>
2019-03-01 10:56:59 +0100

    Product model/seed/json DONE

---
+++ bin/products-seed.js
@@ -0,0 +1,29 @@
+//node bin/products-seed.js Commande terminal
+
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const Product = require("../models/product-model.js");
+const allProduct = require("./products.json");
+
+mongoose
+  .connect("mongodb://localhost/3000", {
+    useNewUrlParser: true
+  })
+  .then(x => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
+
+Product.insertMany(allProduct)
+  .then(productResult => {
+    console.log(`Inserted ${productResult.length} product`);
+  })
+  .catch(err => {
+    console.log("PRODUCT insert error 💩", err);
+  });
--- models/product-model.js
+++ models/product-model.js
@@ -2,7 +2,23 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({}, {});
+const productSchema = new Schema(
+  {
+    gender: { type: String, enum: ["Male", "Female"] },
+    baseImageUrl: { type: String },
+    brandName: { type: String },
+    colour: { type: String },
+    size: { type: Array },
+    productId: { type: Number },
+    name: { type: String },
+    price: { type: Number },
+    productType: { type: String },
+    url: { type: String }
+  },
+  {
+    timestamps: true
+  }
+);
 
 const Product = mongoose.model("Product", productSchema);
 
